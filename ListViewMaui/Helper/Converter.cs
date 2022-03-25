31495e2b9fb195be93d3ab1a033c8b44a5470a56
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2022-03-25 14:22:44 +0530

    Add files via upload

---
+++ ListViewMaui/Helper/Converter.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace ListViewMaui
+{
+    public class ColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return false;
+            var itemdata = value as Contacts;
+            if (itemdata.ContactType == "HOME")
+                return Colors.RoyalBlue;
+            else if (itemdata.ContactType == "WORK")
+                return Colors.PaleGreen;
+            else if (itemdata.ContactType == "MOBILE")
+                return Colors.HotPink;
+            else if (itemdata.ContactType == "OTHER")
+                return Colors.DarkGoldenrod;
+            else
+                return Colors.BlueViolet;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
